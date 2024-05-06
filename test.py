6ed93f680aae8b08a719de01388579af91500752
arch225 <rachit.promo@gmail.com>
2024-05-06 09:04:20 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas
+import numpy as np
+
+#write a code to generate a dataframe with columns: name to be taken from random names from list ('Mukesh', 'Anil', "Narendra", 'Rahul'), income from 0-1500
